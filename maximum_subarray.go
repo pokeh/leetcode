c748e3dd2748e6e89bc60466956aaaab96eaa48c
Chiyo T N <pokeh@users.noreply.github.com>
2019-08-04 14:46:47 -0700

    Create maximum_subarray.go

---
+++ maximum_subarray.go
@@ -0,0 +1,29 @@
+func maxSubArray(nums []int) int {
+    if len(nums) == 1 {
+        return nums[0]
+    }
+    
+    prevMax := maxSubArray(nums[:len(nums)-1])
+    currMax := maxContainLastEl(nums)
+    
+    if currMax > prevMax {
+        return currMax
+    } else {
+        return prevMax
+    }
+}
+
+func maxContainLastEl(nums []int) int {
+    sum := nums[len(nums)-1]
+    max := sum
+    
+    for i := len(nums)-2; i >= 0; i-- {
+        newSum := sum + nums[i]
+        if max < newSum {
+            max = newSum
+        }
+        sum = newSum
+    }
+    
+    return max
+}
