fd1f451ce4ec8bda84d43729489fea0197bc215a
Chiyo T N <pokeh@users.noreply.github.com>
2019-08-05 11:06:14 -0700

    Create best_time_to_buy_and_sell_stock.go

---
+++ best_time_to_buy_and_sell_stock.go
@@ -0,0 +1,29 @@
+// application of Kadane's algorithm
+func maxProfit(prices []int) int {
+    // local max
+    lMax := 0
+    // global max
+    gMax := 0
+    // a temp variable to store the diff between two consecutive prices
+    diff := 0
+    
+    for i := 1; i < len(prices); i++ {
+        diff = prices[i] - prices[i-1]
+        lMax = max(lMax + diff, diff)
+        gMax = max(gMax, lMax)
+    }
+    
+    if gMax < 0 {
+        return 0
+    } else {
+        return gMax
+    }
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    } else {
+        return y
+    }
+}
