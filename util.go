9b7523003ac11af55e70f2379df221c8dabc8017
Chiyo T N <pokeh@users.noreply.github.com>
2019-12-18 18:13:27 -0800

    Create util.go

---
+++ util.go
@@ -0,0 +1,35 @@
+func uniq(nums []int) []int {
+    m := make(map[int]bool)
+
+    res := make([]int, 0, len(nums))
+    
+    for _, n := range nums {
+        if !m[n] {
+            res = append(res, n)
+            m[n] = true
+        }
+    }
+    
+    return res
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -1 * a
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
