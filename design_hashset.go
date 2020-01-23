355081cc4d9260baa46dec7085c45b739cf5195f
Chiyo T N <pokeh@users.noreply.github.com>
2020-01-22 17:06:08 -0800

    Update design_hashset.go
    Convert spaces to tabs

--- design_hashset.go
+++ design_hashset.go
@@ -1,63 +1,63 @@
 const tableSize = 10
 
 type MyHashSet struct {
-    hashTable [tableSize]*Bucket
+	hashTable [tableSize]*Bucket
 }
 
 type Bucket []int
 
 func hash(key int) int {
-    return key % tableSize
+	return key % tableSize
 }
 
 /** Initialize your data structure here. */
 func Constructor() MyHashSet {
-    h := MyHashSet{}
-    
-    for i := 0; i < tableSize; i++ {
-        b := make(Bucket, 0)
-        h.hashTable[i] = &b
-    }
-    
-    return h
+	h := MyHashSet{}
+	
+	for i := 0; i < tableSize; i++ {
+		b := make(Bucket, 0)
+		h.hashTable[i] = &b
+	}
+	
+	return h
 }
 
 func (this *MyHashSet) Add(key int) {
-    bucket := this.hashTable[hash(key)]
-    if !bucket.contains(key) {
-        *bucket = append(*bucket, key)
-    }
+	bucket := this.hashTable[hash(key)]
+	if !bucket.contains(key) {
+		*bucket = append(*bucket, key)
+	}
 }
 
 func (this *MyHashSet) Remove(key int) {
-    bucket := this.hashTable[hash(key)]
-    this.hashTable[hash(key)] = removeKeyFromBucket(key, bucket)
+	bucket := this.hashTable[hash(key)]
+	this.hashTable[hash(key)] = removeKeyFromBucket(key, bucket)
 }
 
 /** Returns true if this set contains the specified element */
 func (this *MyHashSet) Contains(key int) bool {
-    bucket := this.hashTable[hash(key)]
-    return bucket.contains(key)
+	bucket := this.hashTable[hash(key)]
+	return bucket.contains(key)
 }
 
 func (b *Bucket) contains(key int) bool {
-    for _, k := range *b {
-        if key == k {
-            return true
-        }
-    }
-    return false
+	for _, k := range *b {
+		if key == k {
+			return true
+		}
+	}
+	return false
 }
 
 func removeKeyFromBucket(key int, b *Bucket) *Bucket {
-    bucket := *b
-    for i, k := range *b {
-        if key == k {
-            bucket = append(bucket[:i], bucket[i+1:]...)
-            return &bucket
-        }
-    }
-    return b
+	bucket := *b
+	for i, k := range *b {
+		if key == k {
+			bucket = append(bucket[:i], bucket[i+1:]...)
+			return &bucket
+		}
+	}
+	return b
 }
 
 /**
