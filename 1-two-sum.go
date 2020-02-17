674ab752ef01a22866e4fd95d61e74e1b5ce3d30
Chiyo T N <chiyo.nishio@gmail.com>
2020-02-17 11:37:08 -0800

    Add 1

---
+++ 1-two-sum.go
@@ -0,0 +1,29 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	numIdxMap := make(map[int]int)
+
+	for i, n := range nums {
+		if idx, ok := numIdxMap[target-n]; ok {
+			return []int{idx, i}
+		} else {
+			numIdxMap[n] = i
+		}
+	}
+
+	return nil
+}
+
+/* brute force
+func twoSum(nums []int, target int) []int {
+	for i, n1 := range nums {
+		for j, n2 := range nums {
+			if i != j && n1+n2 == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return nil
+}
+*/
