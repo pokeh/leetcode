c8d0aacaaf3150bd67e83c8fb4dec2f1ffc74875
Chiyo T N <ctoyoshima@quoter.com>
2020-04-19 22:50:48 -0700

    Day 19

---
+++ 30-day-challenge/19-search-in-rotated-sorted-array.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	fmt.Println(search(nums, 3))
+}
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		// fmt.Printf("nums[%v]: %v, nums[%v]: %v\n", left, nums[left], right, nums[right])
+		mid := (left + right) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[left] <= nums[mid] {
+			// in left half (no pivot)
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+				continue
+			}
+
+			// in right half (with pivot)
+			left = mid + 1
+			continue
+		}
+
+		// in right half (no pivot)
+		if nums[mid] < target && target <= nums[right] {
+			left = mid + 1
+			continue
+		}
+
+		// in left half (with pivot)
+		right = mid - 1
+	}
+
+	return -1
+}
