586f20c74eea3b1705ed22edff5897711908e10c
Chiyo T N <ctoyoshima@quoter.com>
2020-05-02 11:07:55 -0700

    Day 2

--- may-leetcoding-challenge/02-jewels-and-stones.go
+++ may-leetcoding-challenge/02-jewels-and-stones.go
@@ -1,16 +1,16 @@
 // time complexity: O(n)
 // space complexity: O(n)
 func numJewelsInStones(J string, S string) int {
-    jewels := make(map[rune]bool)
-    for _, j := range J {
-        jewels[j] = true
-    }
+	jewels := make(map[rune]bool)
+	for _, j := range J {
+		jewels[j] = true
+	}
 
-    var cnt int
-    for _, s := range S {
-        if jewels[s] {
-            cnt++
-        }
-    }
-    return cnt
+	var cnt int
+	for _, s := range S {
+		if jewels[s] {
+			cnt++
+		}
+	}
+	return cnt
 }
