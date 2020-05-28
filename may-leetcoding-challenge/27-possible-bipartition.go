55be834c240d94919c9539433ae9e2823d21af81
Chiyo T N <ctoyoshima@quoter.com>
2020-05-27 23:02:03 -0700

    Day 27

--- may-leetcoding-challenge/27-possible-bipartition.go
+++ may-leetcoding-challenge/27-possible-bipartition.go
@@ -1,3 +1,52 @@
+// tactic: create a graph with edges between the dislikes
+// then try to "color the graph by 2 colors", i.e. start off with one vertex as 1,
+// then assign -1 to its adjacent edges, then 1 to the next adjacent... etc
+// if it contradicts at some point, return false
+func possibleBipartition(N int, dislikes [][]int) bool {
+	if len(dislikes) == 0 {
+		return true
+	}
+
+	// adjacent edges
+	adjLists := make(map[int][]int, N)
+
+	for _, d := range dislikes {
+		a, b := d[0], d[1]
+		adjLists[a] = append(adjLists[a], b)
+		adjLists[b] = append(adjLists[b], a)
+	}
+
+	// fmt.Println(adjLists)
+
+	// group is either -1 or 1
+	// length is N+1 so indices can match the edge numbers (we don't use the 0th element)
+	groups := make([]int, N+1)
+
+	for i := 1; i <= N; i++ {
+		if groups[i] == 0 && !dfs(i, 1, adjLists, groups) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func dfs(edge int, group int, adjLists map[int][]int, groups []int) bool {
+	if groups[edge] != 0 {
+		return groups[edge] == group
+	}
+	groups[edge] = group
+
+	for _, adj := range adjLists[edge] {
+		if !dfs(adj, -1*group, adjLists, groups) {
+			return false
+		}
+	}
+
+	return true
+}
+
+/*
 // thanks to https://leetcode.com/problems/possible-bipartition/discuss/543270/golang
 func possibleBipartition(N int, dislikes [][]int) bool {
 	if len(dislikes) == 0 {
@@ -46,3 +95,4 @@ func dfs(i, N, expect int, colors []int, graph [][]bool) bool {
 
 	return true
 }
+*/
