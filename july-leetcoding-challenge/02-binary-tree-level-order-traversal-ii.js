30b5e6bb90358391c57734de9cc15a7863f8e7d1
Chiyo T N <ctoyoshima@quoter.com>
2020-07-03 18:52:01 -0700

    Day 2 BFS

--- july-leetcoding-challenge/02-binary-tree-level-order-traversal-ii.js
+++ july-leetcoding-challenge/02-binary-tree-level-order-traversal-ii.js
@@ -10,6 +10,51 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+var levelOrderBottom = function (root) {
+  if (root === null || root.val === null) {
+    return [];
+  }
+
+  const res = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const numOfNodesInRow = queue.length;
+
+    const row = [];
+
+    for (i = 0; i < numOfNodesInRow; i++) {
+      // dequeue the next node to process
+      const node = queue.shift();
+
+      // process node
+      row.push(node.val);
+      if (node.left != null) {
+        queue.push(node.left);
+      }
+      if (node.right != null) {
+        queue.push(node.right);
+      }
+    }
+
+    res.push(row);
+  }
+
+  return res.reverse();
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
 /*
 var levelOrderBottom = function (root) {
   let res = [];
@@ -32,10 +77,7 @@ var levelOrderBottom = function (root) {
 };
 */
 
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
+/*
 var levelOrderBottom = function (root) {
   const res = [];
 
@@ -57,3 +99,4 @@ var levelOrderBottom = function (root) {
 
   return res.reverse();
 };
+*/
