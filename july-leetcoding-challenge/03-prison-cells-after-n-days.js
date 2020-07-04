d57a239158ca6b1b0bc50ecfa63195a149be95f9
Chiyo T N <ctoyoshima@quoter.com>
2020-07-03 22:57:27 -0700

    Day 3

--- july-leetcoding-challenge/03-prison-cells-after-n-days.js
+++ july-leetcoding-challenge/03-prison-cells-after-n-days.js
@@ -1,11 +1,35 @@
 "use strict";
 
-/**
- * @param {number[]} cells
- * @param {number} N
- * @return {number[]}
- */
+var prisonAfterNDays = function (cells, N) {
+  const res = Array(cells.length).fill(0);
+
+  const history = [];
+
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j < cells.length - 1; j++) {
+      res[j] = cells[j - 1] === cells[j + 1] ? 1 : 0;
+    }
+
+    if (history.includes(res.join(""))) {
+      const idx = N % history.length;
+
+      if (idx === 0) {
+        return history[history.length - 1].split("");
+      }
+
+      return history[idx - 1].split("");
+    }
+
+    history.push(res.join(""));
+
+    cells = [...res];
+  }
+
+  return res;
+};
+
 // TLE
+/*
 var prisonAfterNDays = function (cells, N) {
   const res = Array(cells.length).fill(0);
 
@@ -19,5 +43,6 @@ var prisonAfterNDays = function (cells, N) {
 
   return res;
 };
+*/
 
 console.log(prisonAfterNDays([0, 1, 1, 0], 3));
